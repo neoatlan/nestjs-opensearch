3f45a643665eb58424e118cceafe3afef26a8c5a
NeoAtlan <neoatlan@neoatlan.net>
2022-12-11 20:35:48 +0900

    Add imports to forRootAsync

--- lib/opensearch.module.ts
+++ lib/opensearch.module.ts
@@ -16,6 +16,9 @@ type ClientMap = Map<string | symbol | undefined, OpensearchClient>;
   ],
 })
 export class OpensearchModule implements OnApplicationShutdown {
+  public static forRoot(options: OpensearchClientOptions): DynamicModule;
+  /** @deprecated Please call forRoot() multiple times instead of using an array */
+  public static forRoot(options: OpensearchClientOptions[]): DynamicModule;
   public static forRoot(options: OpensearchClientOptions | OpensearchClientOptions[]): DynamicModule {
     const providers = OpensearchModule.buildProviders(options);
     return {
@@ -25,10 +28,14 @@ export class OpensearchModule implements OnApplicationShutdown {
     };
   }
 
+  public static forRootAsync(options: OpensearchAsyncClientOptions): DynamicModule;
+  /** @deprecated Please call forRootAsync() multiple times instead of using an array */
+  public static forRootAsync(options: OpensearchAsyncClientOptions[]): DynamicModule;
   public static forRootAsync(options: OpensearchAsyncClientOptions | OpensearchAsyncClientOptions[]): DynamicModule {
     const providers = OpensearchModule.buildAsyncProviders(options);
     return {
       module: OpensearchModule,
+      imports: Array.isArray(options) ? undefined : options.imports,
       exports: providers,
       providers,
     };
