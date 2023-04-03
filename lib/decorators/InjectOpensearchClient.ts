6fe8c0e90ad9326a01045a58feaf6fe7e4a86588
Vladyslav Zavalykhatko <vladhatko@gmail.com>
2023-04-03 17:24:47 +0200

    set explicit return type on InjectOpensearchClient

--- lib/decorators/InjectOpensearchClient.ts
+++ lib/decorators/InjectOpensearchClient.ts
@@ -2,5 +2,5 @@ import { Inject } from '@nestjs/common';
 import { buildInjectionToken } from '../helpers';
 import { OpensearchClient } from '../opensearch-client';
 
-export const InjectOpensearchClient = (clientName?: string | symbol) =>
+export const InjectOpensearchClient = (clientName?: string | symbol): ParameterDecorator =>
   Inject(clientName ? buildInjectionToken(clientName) : OpensearchClient);
