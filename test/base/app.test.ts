05171287a10ae33d59a462f0a51ce1f338e16d24
NeoAtlan <neoatlan@neoatlan.net>
2024-09-01 22:28:11 +0900

    Add a test case

--- test/base/app.test.ts
+++ test/base/app.test.ts
@@ -3,7 +3,13 @@
 
 import { Injectable, Module, Optional } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { InjectOpensearchClient, OpensearchClient, OpensearchModule } from 'nestjs-opensearch';
+import { ClientOptions } from '@opensearch-project/opensearch';
+import {
+  InjectOpensearchClient,
+  OpensearchClient,
+  OpensearchModule,
+  OpensearchClientOptionsFactory,
+} from 'nestjs-opensearch';
 import { getClientName } from 'nestjs-opensearch/helpers';
 
 @Injectable()
@@ -43,6 +49,24 @@ class ConfigModuleA { }
   exports: [ 'node' ],
 })
 class ConfigModuleB { }
+
+@Injectable()
+class ConfigServiceA implements OpensearchClientOptionsFactory {
+  public createOpensearchClientOptions(): ClientOptions {
+    return {
+      node: 'http://localhost:9201',
+    };
+  }
+}
+
+@Injectable()
+class ConfigServiceB implements OpensearchClientOptionsFactory {
+  public createOpensearchClientOptions(): ClientOptions {
+    return {
+      node: 'http://localhost:9202',
+    };
+  }
+}
 
 describe('Client injections', () => {
   let _tm: TestingModule | null = null;
@@ -234,5 +258,29 @@ describe('Client injections', () => {
     expect(getClientName(ss.defaultClient!)).toBeUndefined();
     expect(getClientName(ss.defaultClient1!)).toBeUndefined();
     expect(getClientName(ss.barClient!)).toEqual('bar');
+  });
+
+  test('default client and bar named client by async module with useClass', async () => {
+    const testModule = _tm = await Test.createTestingModule({
+      imports: [
+        OpensearchModule.forRootAsync({
+          useClass: ConfigServiceA,
+        }),
+        OpensearchModule.forRootAsync({
+          clientName: 'foo',
+          useClass: ConfigServiceB,
+        }),
+      ],
+      providers: [ SearchService ],
+    }).compile();
+
+    const ss = testModule.get(SearchService);    
+    expect(ss.defaultClient).not.toBeUndefined();
+    expect(ss.defaultClient1).not.toBeUndefined();
+    expect(ss.fooClient).not.toBeUndefined();
+    expect(ss.barClient).toBeUndefined();
+    expect(getClientName(ss.defaultClient!)).toBeUndefined();
+    expect(getClientName(ss.defaultClient1!)).toBeUndefined();
+    expect(getClientName(ss.fooClient!)).toEqual('foo');
   });
 });
